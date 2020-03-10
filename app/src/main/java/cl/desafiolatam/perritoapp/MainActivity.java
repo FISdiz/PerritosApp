706ca866bd73d89881c3c595f5155575ddf0bdde
Felipe <felipe.salazar.di@gmail.com>
2020-03-10 15:35:16 -0300

    RecycleView - Adapter pendiente

--- app/src/main/java/cl/desafiolatam/perritoapp/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/perritoapp/MainActivity.java
@@ -1,6 +1,8 @@
 package cl.desafiolatam.perritoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        /*
+        RecyclerView dogsRecycleView = findViewById(R.id.dogList);
+        RecyclerView.LayoutManager linearList = new LinearLayoutManager();
+        dogsRecycleView.setLayoutManager(linearList);
+        dogsRecycleView.setAdapter(// incorporar adaptador);
+        */
     }
 }
