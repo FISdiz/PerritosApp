05d796620d6e08196e4e4c907a69edd3c2dfd40e
Gary Hermosilla <garuxa@gmail.com>
2020-03-10 16:44:04 -0300

    Agregado RazaApi Omitir clases Callhandle, RequesInterfaceApi, Response

--- app/src/main/java/cl/desafiolatam/perritoapp/api/RazaApi.java
+++ app/src/main/java/cl/desafiolatam/perritoapp/api/RazaApi.java
@@ -1,11 +1,13 @@
 package cl.desafiolatam.perritoapp.api;
 
+import java.util.List;
+
 import cl.desafiolatam.perritoapp.model.Raza;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RazaApi {
 
-    //@GET("api/breeds/list/all")TODO Corregir
-    //Call<Raza>;TODO Corregir
+    @GET("api/breeds/list/all")
+    Call<Raza> getAllBreeds();
 }
